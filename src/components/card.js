15b28b505af1b879b030443f210afdd3427c7ca1
Mauritz Marais <mauritz303@gmail.com>
2023-05-05 16:40:30 +0200

    added function to shuffle array and display cards randomly

--- src/components/card.js
+++ src/components/card.js
@@ -1,8 +1,9 @@
-// render a card for each card in object
+// render a card for each card in array
 function Card({ cardList, handleClick }) {
-    // display cards in random order*
+    // display cards in random order
+    const cardListShuffled = shuffleArray(cardList);
 
-    const cards = cardList.map(card => {
+    const cards = cardListShuffled.map(card => {
         return <div key={card.id} className="card-item" onClick={() => handleClick(card.id)}>
             <img src={card.src} alt={card.name} />
         </div>
@@ -16,3 +17,12 @@ function Card({ cardList, handleClick }) {
 }
 
 export default Card;
+
+// algorithm to shuffle array (Durstenfeld)
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
