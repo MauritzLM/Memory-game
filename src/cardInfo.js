10123ed9214ff123611320bbeed4bc29d8bdcb53
Mauritz Marais <mauritz303@gmail.com>
2023-05-05 16:37:53 +0200

    added more cards

--- src/cardInfo.js
+++ src/cardInfo.js
@@ -3,11 +3,29 @@ import painting1 from './images/painting1.png';
 import painting2 from './images/painting2.png';
 import painting3 from './images/painting3.png';
 import painting4 from './images/painting4.png';
+import painting5 from './images/painting5.png';
+import painting6 from './images/painting6.png';
+import painting7 from './images/painting7.png';
+import painting8 from './images/painting8.png';
+import painting9 from './images/painting9.png';
+import painting10 from './images/painting10.png';
+import painting11 from './images/painting11.png';
+import painting12 from './images/painting12.png';
+
+
 
 // array of cards
 export const cards = [
     { id: uuidv4(), src: painting1, name: 'painting-1', hasBeenClicked: false },
     { id: uuidv4(), src: painting2, name: 'painting-2', hasBeenClicked: false },
     { id: uuidv4(), src: painting3, name: 'painting-3', hasBeenClicked: false },
-    { id: uuidv4(), src: painting4, name: 'painting-4', hasBeenClicked: false }
+    { id: uuidv4(), src: painting4, name: 'painting-4', hasBeenClicked: false },
+    { id: uuidv4(), src: painting5, name: 'painting-5', hasBeenClicked: false },
+    { id: uuidv4(), src: painting6, name: 'painting-6', hasBeenClicked: false },
+    { id: uuidv4(), src: painting7, name: 'painting-7', hasBeenClicked: false },
+    { id: uuidv4(), src: painting8, name: 'painting-8', hasBeenClicked: false },
+    { id: uuidv4(), src: painting9, name: 'painting-9', hasBeenClicked: false },
+    { id: uuidv4(), src: painting10, name: 'painting-10', hasBeenClicked: false },
+    { id: uuidv4(), src: painting11, name: 'painting-11', hasBeenClicked: false },
+    { id: uuidv4(), src: painting12, name: 'painting-12', hasBeenClicked: false }
 ]
