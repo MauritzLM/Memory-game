9133d590f2602fe838c0251f869e51c49d8d06b8
Mauritz Marais <mauritz303@gmail.com>
2023-05-06 10:42:09 +0200

    added high score

--- src/components/scoreboard.js
+++ src/components/scoreboard.js
@@ -1,7 +1,8 @@
-function ScoreBoard({ score }) {
+function ScoreBoard({ score, highScore }) {
     return (
         <div className="score-board">
             <p>Current score:{score}</p>
+            <p>High score: {highScore}</p>
         </div>
     )
 }
